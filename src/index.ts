632626b6e584c21743b77d87ac7039417725ae75
Ponomarev Stepan <htconesuper11@gmail.com>
2020-04-19 03:55:04 +0300

    Proxy was added

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,26 @@ import { TelegramClientImpl } from './telegram/client';
 import { UserUsecaseImpl } from './user/usecase/usecase';
 */
 import Telegraf from 'telegraf';
+import SocksAgent from 'socks5-https-client/lib/Agent';
+
 
 //const telegramClient = new TelegramClientImpl();
 //const userUsecase = new UserUsecaseImpl();
 
 //startHandle(telegramClient, userUsecase);
 
+
+const socksAgent = new SocksAgent({
+  socksHost: '127.0.0.1',
+  socksPort: '9150',
+});
+
+
 const TELEGRAM_BOT_API_KEY = '1247273754:AAG_kYMP1MVTFMCm2sdCGHAAFJzyLqsoR_8';
 
-const bot = new Telegraf(TELEGRAM_BOT_API_KEY);
+const bot = new Telegraf(TELEGRAM_BOT_API_KEY, {
+    telegram: { agent: socksAgent }
+});
 
 bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
